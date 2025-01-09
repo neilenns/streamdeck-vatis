27922c04970cb1d3dc9bf0a96bb4f11381176c6a
Neil Enns <neile@live.com>
2025-01-09 10:16:07 -0800

    Fix logging

--- src/events/streamdeck/applicationDidLaunch.ts
+++ src/events/streamdeck/applicationDidLaunch.ts
@@ -2,7 +2,7 @@ import { ApplicationDidLaunchEvent } from "@elgato/streamdeck";
 import mainLogger from "@utils/logger";
 import vAtisManager from "@managers/vatis";
 
-const logger = mainLogger.child({ service: "plugin" });
+const logger = mainLogger.child({ service: "applicationDidLaunch" });
 
 export const handleOnApplicationDidLaunch = (ev: ApplicationDidLaunchEvent) => {
   logger.info("Received applicationDidLaunch event", ev.application);
--- src/events/streamdeck/applicationDidTerminate.ts
+++ src/events/streamdeck/applicationDidTerminate.ts
@@ -2,7 +2,7 @@ import { ApplicationDidTerminateEvent } from "@elgato/streamdeck";
 import mainLogger from "@utils/logger";
 import vAtisManager from "@managers/vatis";
 
-const logger = mainLogger.child({ service: "plugin" });
+const logger = mainLogger.child({ service: "handleOnApplicationDidTerminate" });
 
 export const handleOnApplicationDidTerminate = (
   ev: ApplicationDidTerminateEvent
